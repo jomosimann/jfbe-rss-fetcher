747e7728c22b6feb7567ff8288bac0046020e8b0
jomosimann <skayjobra@gmx.ch>
2026-04-06 20:07:18 +0200

    Editet Next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig  = {
+  experimental: {
+    serverComponentsExternalPackages: ['jsdom', '@mozilla/readability'],
+  },
+}
+module.exports = nextConfig
 
 export default nextConfig;
